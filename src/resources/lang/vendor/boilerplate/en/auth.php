d8490e331dfc5ccafd7bd2dc575e5b6b03bc6c57
sebastienheyd <sebastienheyd@github.com>
2017-04-01 09:51:40 +0200

    EN locales

---
+++ src/resources/lang/vendor/boilerplate/en/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'fields' => [
+        'last_name'         => 'Last name',
+        'first_name'        => 'First name',
+        'email'             => 'E-mail',
+        'password'          => 'Password',
+        'password_confirm'  => 'Password confirmation',
+    ],
+    'password' => [
+        'title'             => 'Forgotten password',
+        'intro'             => 'Enter the following field to reset your password',
+        'submit'            => 'Send reset link',
+        'login_link'        => "Sign in with an existing user",
+    ],
+    'password_reset' => [
+        'title'             => 'Password reset',
+        'intro'             => 'Enter the following fields to reset your password',
+        'submit'            => 'Reset',
+    ],
+    'register' => [
+        'title'             => 'Register a new user',
+        'intro'             => 'Enter the following fields to create a new user',
+        'register_button'   => 'Register',
+        'login_link'        => "Sign in with an existing user",
+    ],
+    'login' => [
+        'title'             => 'Login',
+        'intro'             => 'Sign in to start your session',
+        'rememberme'        => 'Remember me',
+        'signin'            => 'Sign in',
+        'forgotpassword'    => "I forgot my password",
+        'register'          => 'Register a new user'
+    ]
+];
---
+++ src/resources/lang/vendor/boilerplate/en/layout.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'mainmenu' => 'Main menu',
+    'logout' => 'Logout',
+    'home'  => 'Home',
+    'dashboard'  => 'Dashboard',
+    'administration'  => 'Administration',
+    'role_managament'  => 'Role management',
+    'user_managament'  => 'User management',
+];
