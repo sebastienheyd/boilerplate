d9257baa4aa6f308ed5199685a6fff25252d631b
sebastienheyd <sebastienheyd@github.com>
2017-10-06 09:17:55 +0200

    Default prefix to avoid conflict with the default laravel's route /

--- src/config/boilerplate/app.php
+++ src/config/boilerplate/app.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'vendorname' => 'Boilerplate', // Vendor name
-    'vendorlink' => '', // Vendor URL
-    'logo-lg'    => '<b>BO</b>ilerplate', // Logo displayed on large screen (title on the top of the main menu)
-    'logo-mini'  => 'BO', // Logo displayed on small screen
-    'prefix'     => '', // Backend prefix. Ex: "admin" => "http://..../admin"
+    'vendorname' => 'Boilerplate',              // Vendor name
+    'vendorlink' => '',                         // Vendor URL
+    'logo-lg'    => '<b>BO</b>ilerplate',       // Logo displayed on large screen (title on the top of the main menu)
+    'logo-mini'  => 'BO',                       // Logo displayed on small screen
+    'prefix'     => 'admin',                    // Backend prefix. Ex: "admin" => "http://..../admin"
     'redirectTo' => 'boilerplate.home'          // Redirection to route after backend login
 ];
