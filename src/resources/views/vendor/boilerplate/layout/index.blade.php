d01984ddbaa91ba707986df36f3a1d7964cf15e4
Sebastien HEYD <sebastienheyd@users.noreply.github.com>
2018-02-14 11:39:00 +0100

    Add ajaxSetup

--- src/resources/views/vendor/boilerplate/layout/index.blade.php
+++ src/resources/views/vendor/boilerplate/layout/index.blade.php
@@ -26,6 +26,7 @@
     <script src="{{ asset('/js/boilerplate.js') }}"></script>
     <script>
         $(function() {
+            $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
             bootbox.setLocale("{{ App::getLocale() }}");
             @if(Session::has('growl'))
                 @if(is_array(Session::get('growl')))
