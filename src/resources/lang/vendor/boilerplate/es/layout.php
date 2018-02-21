640ee0d3da0df78fb1b90eb1672c026236bcb80c
Dani <daniel@sonica.es>
2018-02-21 20:04:48 +0100

    Added spanish language files

--- src/resources/lang/vendor/boilerplate/es/layout.php
+++ src/resources/lang/vendor/boilerplate/es/layout.php
@@ -3,11 +3,11 @@
 return [
     'mainmenu'          => 'Menú principal',
     'online'            => 'Connectado',
-    'logout'            => 'Desconectad',
+    'logout'            => 'Desconectar',
     'home'              => 'Inicio',
     'dashboard'         => 'Panel de gestión',
     'access'            => 'Gestión de acceso',
-    'role_management'   => 'Gestión de rooles',
+    'role_management'   => 'Gestión de roles',
     'user_management'   => 'Gestión de usuarios',
     'logoutconfirm'     => '¿Estas seguro que deseas salir de la sesión?',
     'rightsres'         => 'Todos los derechos reservados.',
