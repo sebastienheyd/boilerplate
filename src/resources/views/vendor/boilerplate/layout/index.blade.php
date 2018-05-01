8b3f4561c95d9b8a5a06699049b05d5d4272c322
sebastienheyd <sebastien.heyd@free.fr>
2018-05-01 07:32:31 +0200

    Meta robots by default to noindex, nofollow

--- src/resources/views/vendor/boilerplate/layout/index.blade.php
+++ src/resources/views/vendor/boilerplate/layout/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }} | {{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/boilerplate.min.css') }}">
