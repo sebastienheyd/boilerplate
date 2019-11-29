d40657935f9feebb5a7951f3d0aa70b78dad13f5
Sebastien HEYD <sebastien.heyd@free.fr>
2019-11-29 17:32:43 +0100

    TinyMCE imagetools / removed menubar

--- src/resources/views/load/tinymce.blade.php
+++ src/resources/views/load/tinymce.blade.php
@@ -3,12 +3,11 @@
         <script src="{!! mix('/js/tinymce/tinymce.min.js', '/assets/vendor/boilerplate') !!}"></script>
         <script>
             tinymce.defaultSettings = {
-                plugins: ["autoresize", "fullscreen", "codemirror", "link"],
-                codemirror: {
-                    config: {
-                        theme: 'storm'
-                    }
-                },
+                plugins: "autoresize fullscreen codemirror link lists table media image imagetools",
+                toolbar: "code fullscreen | undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | table | bullist numlist outdent indent | link media image",
+                codemirror: { config: { theme: 'storm' } },
+                menubar: false,
+                removed_menuitems: 'newdocument',
                 remove_linebreaks: false,
                 forced_root_block: false,
                 verify_html: false,
