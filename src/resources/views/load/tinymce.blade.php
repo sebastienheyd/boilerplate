9ea095cc1708afa55c4933c45bc7b309002a434e
Sebastien HEYD <sebastien.heyd@free.fr>
2020-01-13 11:02:43 +0100

    Tinymce fast paste cleanup

--- src/resources/views/load/tinymce.blade.php
+++ src/resources/views/load/tinymce.blade.php
@@ -3,9 +3,11 @@
         <script src="{!! mix('/js/tinymce/tinymce.min.js', '/assets/vendor/boilerplate') !!}"></script>
         <script>
             tinymce.defaultSettings = {
-                plugins: "autoresize fullscreen codemirror link lists table media image imagetools customalign stickytoolbar",
+                plugins: "autoresize fullscreen codemirror link lists table media image imagetools paste customalign stickytoolbar",
                 toolbar: "undo redo | styleselect | bold italic | customalignleft aligncenter customalignright | link media image | bullist numlist | table | code fullscreen",
+                contextmenu: "link image imagetools table spellchecker removeformat",
                 sticky_toolbar_container: '.tox-editor-header',
+                toolbar_drawer: "sliding",
                 sticky_offset: $('nav.main-header').outerHeight(),
                 codemirror: { config: { theme: 'storm' } },
                 menubar: false,
@@ -18,6 +20,11 @@
                 browser_spellcheck: true,
                 encoding: 'UTF-8',
                 image_uploadtab: false,
+                paste_preprocess: function(plugin, args) {
+                    args.content = args.content.replace(/<(\/)*(\\?xml:|meta|link|span|font|del|ins|st1:|[ovwxp]:)((.|\s)*?)>/gi, ''); // Unwanted tags
+                    args.content = args.content.replace(/\s(class|style|type|start)=("(.*?)"|(\w*))/gi, ''); // Unwanted attributes
+                    args.content = args.content.replace(/<(p|a|div|span|strike|strong|i|u)[^>]*?>(\s|&nbsp;|<br\/>|\r|\n)*?<\/(p|a|div|span|strike|strong|i|u)>/gi, ''); // Empty tags
+                },
                 @if(config('boilerplate.app.locale') !== 'en')
                 language: '{{ config('boilerplate.app.locale') }}'
                 @endif
