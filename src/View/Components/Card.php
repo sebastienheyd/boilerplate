78feb496ddcd72a7ccf19b59a508585e747ce47e
Sebastien HEYD <sebastien.heyd@free.fr>
2021-04-06 14:13:51 +0200

    Card component

--- src/View/Components/Card.php
+++ src/View/Components/Card.php
@@ -6,6 +6,17 @@ use Illuminate\View\Component;
 
 class Card extends Component
 {
+    public $color;
+    public $title;
+    public $tabs;
+
+    public function __construct($color = null, $title = null, $tabs = false)
+    {
+        $this->color = $color ?? config('boilerplate.theme.card.default_color', 'info');
+        $this->title = $title;
+        $this->tabs = $tabs ? true : null;
+    }
+
     /**
      * Get the view / contents that represent the component.
      *
