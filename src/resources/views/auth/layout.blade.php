1ead82c153c752018bf2b58ec9cb62cfd5ed7559
Sebastien HEYD <sebastien.heyd@free.fr>
2021-05-07 08:14:15 +0200

    Font Source Sans Pro on auth pages

--- src/resources/views/auth/layout.blade.php
+++ src/resources/views/auth/layout.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }} | {{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ mix('/adminlte.min.css', '/assets/vendor/boilerplate') }}">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
 </head>
 <body class="hold-transition {{ $bodyClass ?? 'login-page'}}">
     @yield('content')
