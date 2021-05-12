fe3665b2ad585e8d688cafe3993422de3626977f
Sebastien HEYD <sebastien.heyd@free.fr>
2021-05-12 21:49:36 +0200

    Default values select2

--- src/resources/views/load/select2.blade.php
+++ src/resources/views/load/select2.blade.php
@@ -5,6 +5,7 @@
 @push('js')
     <script src="{!! mix('/plugins/select2/select2.full.min.js', '/assets/vendor/boilerplate') !!}"></script>
     <script src="{!! mix('/plugins/select2/i18n/'.config('boilerplate.app.locale').'.js', '/assets/vendor/boilerplate') !!}"></script>
+    <script>$.extend(true,$.fn.select2.defaults,{language:'{{ config('boilerplate.app.locale') }}',direction:'@lang('boilerplate::layout.direction')'});</script>
 @endpush
 @php(define('LOAD_SELECT2', true))
 @endif
