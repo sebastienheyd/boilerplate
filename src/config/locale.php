b80d110647d278a0c0be2a49daef3239fdadf867
Sébastien HEYD <sebastien.heyd@free.fr>
2021-08-12 16:44:29 +0200

    Bulgarian language switch

--- src/config/locale.php
+++ src/config/locale.php
@@ -3,8 +3,9 @@
 return [
     'default' => config('app.locale'),
     'switch' => false,
-    'allowed' => ['en', 'es', 'fa', 'fr', 'it', 'tr'],
+    'allowed' => ['bg', 'en', 'es', 'fa', 'fr', 'it', 'tr'],
     'languages' => [
+        'bg' => ['label' => 'български', 'datatable' => 'Bulgarian'],
         'en' => ['label' => 'English', 'datatable' => 'English'],
         'es' => ['label' => 'Español', 'datatable' => 'Spanish'],
         'fa' => ['label' => 'فارسی', 'datatable' => 'Persian'],
