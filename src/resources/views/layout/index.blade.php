1dd6f72873e744dab747fe12587e6c2781ed9a45
Sebastien HEYD <sebastien.heyd@free.fr>
2021-09-01 14:44:47 +0200

    Moved css stack

--- src/resources/views/layout/index.blade.php
+++ src/resources/views/layout/index.blade.php
@@ -7,12 +7,12 @@
     <meta name="robots" content="noindex, nofollow">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }} | {{ config('app.name') }}</title>
-@stack('css')
     <link rel="stylesheet" href="{{ mix('/plugins/fontawesome/fontawesome.min.css', '/assets/vendor/boilerplate') }}">
     <link rel="stylesheet" href="{{ mix('/adminlte.min.css', '/assets/vendor/boilerplate') }}">
     <link rel="shortcut icon" href="{{ config('boilerplate.theme.favicon') ?? mix('favicon.svg', '/assets/vendor/boilerplate') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
+@stack('css')
 </head>
 <body class="layout-fixed layout-navbar-fixed sidebar-mini">
     <div class="wrapper">
