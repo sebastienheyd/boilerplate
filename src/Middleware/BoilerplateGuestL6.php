b82444faa24fa664269f0b8d42d4f4cafc29c241
Sébastien HEYD <sebastien.heyd@free.fr>
2021-10-21 16:19:43 +0200

    Fix StyleCI

--- src/Middleware/BoilerplateGuestL6.php
+++ src/Middleware/BoilerplateGuestL6.php
@@ -13,7 +13,6 @@ class BoilerplateGuestL6
      *
      * @param  Request  $request
      * @param  Closure  $next
-     *
      * @return mixed
      */
     public function handle($request, Closure $next)
