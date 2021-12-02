53084990b19359f6229b9c81154ca69d0c29c715
Sébastien HEYD <sebastien.heyd@free.fr>
2021-12-02 11:18:59 +0100

    Assets update

--- src/resources/views/stubs/columns/date.blade.php
+++ src/resources/views/stubs/columns/date.blade.php
@@ -1,3 +1,4 @@
 Column::add(__('{{ $title }}'))
+                ->width('180px')
                 ->data('{{ $field }}')
                 ->dateFormat({{ $type === 'date' ? '__("boilerplate::date.Ymd")' : '' }}),
